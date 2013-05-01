Root-Box/device_samsung_qcom-common

PowerHAL: Only write to SAMPLING_RATE_ONDEMAND when using Ondemand
Prevents E/CM PowerHAL(  559): Error opening /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate: No such file or directory

Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>

power/power.c
@@ -178,13 +178,23 @@ static void cm_power_hint(struct power_module *module, power_hint_t hint,
 
 static void cm_power_set_interactive(struct power_module *module, int on)
 {
-    sysfs_write(SAMPLING_RATE_ONDEMAND,
-            on ? SAMPLING_RATE_SCREEN_ON : SAMPLING_RATE_SCREEN_OFF);
+    char governor[80];
+
+    if (strncmp(governor, "ondemand", 8) == 0)
+        sysfs_write(SAMPLING_RATE_ONDEMAND,
+                on ? SAMPLING_RATE_SCREEN_ON : SAMPLING_RATE_SCREEN_OFF);
+    else
+        ALOGW("Skipping sysfs_write to sampling_rate -- NOT using ondemand");
 }
 
 static void cm_power_init(struct power_module *module)
 {
-    sysfs_write(SAMPLING_RATE_ONDEMAND, SAMPLING_RATE_SCREEN_ON);
+    char governor[80];
+
+    if (strncmp(governor, "ondemand", 8) == 0)
+        sysfs_write(SAMPLING_RATE_ONDEMAND, SAMPLING_RATE_SCREEN_ON);
+    else
+        ALOGW("Skipping sysfs_write to sampling_rate -- NOT using ondemand");
 }
 
 static struct hw_module_methods_t power_module_methods = {

ParanoidAndroid-Xperia/android_device_semc_msm7x30-common

Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
Change-Id: I563942194ae95758b45ed9aa76f24f96b28ff4d4
Signed-off-by: garwedgess <garethwilliams21@gmail.com>

power/power.c
@@ -178,13 +178,23 @@ static void cm_power_hint(struct power_module *module, power_hint_t hint,
 
 static void cm_power_set_interactive(struct power_module *module, int on)
 {
-    sysfs_write(SAMPLING_RATE_ONDEMAND,
-            on ? SAMPLING_RATE_SCREEN_ON : SAMPLING_RATE_SCREEN_OFF);
+    char governor[80];
+
+    if (strncmp(governor, "ondemand", 8) == 0)
+        sysfs_write(SAMPLING_RATE_ONDEMAND,
+                on ? SAMPLING_RATE_SCREEN_ON : SAMPLING_RATE_SCREEN_OFF);
+    else
+        ALOGV("Skipping sysfs_write to sampling_rate -- NOT using ondemand");
 }
 
 static void cm_power_init(struct power_module *module)
 {
-    sysfs_write(SAMPLING_RATE_ONDEMAND, SAMPLING_RATE_SCREEN_ON);
+    char governor[80];
+
+    if (strncmp(governor, "ondemand", 8) == 0)
+        sysfs_write(SAMPLING_RATE_ONDEMAND, SAMPLING_RATE_SCREEN_ON);
+    else
+        ALOGV("Skipping sysfs_write to sampling_rate -- NOT using ondemand");
 }
 
 static struct hw_module_methods_t power_module_methods = {